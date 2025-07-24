edc355801a52444e885219994aefe29dad5cf5b1
axel604 <axel@kleinblue.fr>
2025-07-24 17:34:16 +0200

    Add files via upload
    maj2

--- fix_links.py
+++ fix_links.py
@@ -1,41 +1,37 @@
 import os
 import re
 
-def patch_img_links(filepath):
+def patch_img_links(filepath, relpath):
     with open(filepath, "r", encoding="utf-8") as f:
         content = f.read()
+    patched = content
 
-    # Patch tous les chemins Windows absolus pour images
-    content_patched = re.sub(
-        r'src="C:\\\\Users\\\\sisit\\\\Desktop\\\\Virgilesite\\\\images\\\\([^"]+)"',
-        r'src="images/\1"',
-        content
-    )
-    # Patch aussi les variantes sans échappement double
-    content_patched = re.sub(
-        r'src="C:\\Users\\sisit\\Desktop\\Virgilesite\\images\\([^"]+)"',
-        r'src="images/\1"',
-        content_patched
-    )
+    # Compte le niveau de profondeur du fichier
+    depth = relpath.count(os.sep)
+    # On ne patch que si ce n'est pas à la racine (root)
+    if depth > 0:
+        # Pour chaque <img src="images/...">, on ajoute les ../ nécessaires
+        patched = re.sub(
+            r'src="images/', f'src="{"../" * depth}images/', patched
+        )
+        # Pour chaque <img src="favicon/...
+        patched = re.sub(
+            r'src="favicon/', f'src="{"../" * depth}favicon/', patched
+        )
+        # Tu peux ajouter d'autres patchs si tu as d'autres dossiers d'assets
 
-    # Patch aussi pour les balises <img src='...'>
-    content_patched = re.sub(
-        r"src='C:\\Users\\sisit\\Desktop\\Virgilesite\\images\\([^']+)'",
-        r"src='images/\1'",
-        content_patched
-    )
-
-    if content != content_patched:
+    if patched != content:
         print(f"Patched: {filepath}")
         with open(filepath, "w", encoding="utf-8") as f:
-            f.write(content_patched)
+            f.write(patched)
 
 def patch_all_html(root):
     for dirpath, dirnames, filenames in os.walk(root):
         for file in filenames:
             if file.endswith(".html"):
-                patch_img_links(os.path.join(dirpath, file))
+                relpath = os.path.relpath(os.path.join(dirpath, file), root)
+                patch_img_links(os.path.join(dirpath, file), relpath)
 
 if __name__ == "__main__":
     patch_all_html(".")
-    print("Tous les liens d’images ont été patchés !")
+    print("Tous les chemins d’images ont été adaptés selon la profondeur du fichier !")
